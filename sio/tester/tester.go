2a057a35e26dd2bc41634e631da5914c54696828
Jeff Horn <jeff@everlook.net>
2016-01-20 13:44:50 -0800

    Add random value for gui values
    Signed-off-by: Jeff Horn <jeff@everlook.net>

--- sio/tester/tester.go
+++ sio/tester/tester.go
@@ -3,11 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/jmore-reachtech/serial"
 	"log"
+	"math/rand"
 	"os"
 	"time"
+
+	"github.com/jmore-reachtech/serial"
 )
+
+const dur int = 10
+
+func random(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(max-min) + min
+}
 
 func main() {
 	ttyPtr := flag.String("tty", "/dev/ttymxc1", "Open tty device")
@@ -24,35 +33,64 @@ func main() {
 	if err != nil {
 		log.Fatal("fatal error: ", err)
 	}
-
 	defer s.Close()
 
-	for j := 0; j < 10; j++ {
+	for j := 0; j < 100; j++ {
 
-		for i := 0; i < 12; i++ {
-			msg := fmt.Sprintf("sl=%d\n", i*10)
-			fmt.Println("sending: ", msg)
+		for i := 0; i < 18; i++ {
+			w := fmt.Sprintf("w=%d\n", random(0, i+1))
+			f := fmt.Sprintf("f=%d\n", random(0, i+1))
+			t := fmt.Sprintf("t=%d\n", random(0, i+1))
+			p := fmt.Sprintf("s=%d\n", random(0, (i+1)*10))
+			o := fmt.Sprintf("o=%d\n", i%2)
 
-			n, err := s.Write([]byte(msg))
-			if nil != err {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			fmt.Printf("wrote %d bytes \n", n)
-			time.Sleep(50 * time.Millisecond)
+			_, err := s.Write([]byte(w))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(f))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(t))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(o))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(p))
+			checkError(err)
+
 		}
 
-		for i := 12; i > 0; i-- {
-			msg := fmt.Sprintf("sl=%d\n", i*10)
-			fmt.Println("sending: ", msg)
+		for i := 18; i > 0; i-- {
+			w := fmt.Sprintf("w=%d\n", random(0, i+1))
+			f := fmt.Sprintf("f=%d\n", random(0, i+1))
+			t := fmt.Sprintf("t=%d\n", random(0, i+1))
+			p := fmt.Sprintf("s=%d\n", random(0, (i+1)*10))
+			o := fmt.Sprintf("o=%d\n", i%2)
 
-			n, err := s.Write([]byte(msg))
-			if nil != err {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			fmt.Printf("wrote %d bytes \n", n)
-			time.Sleep(50 * time.Millisecond)
+			_, err := s.Write([]byte(w))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(f))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(t))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(o))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
+			_, err = s.Write([]byte(p))
+			checkError(err)
+			time.Sleep((time.Duration(dur) * time.Millisecond))
 		}
+	}
+}
+
+// If err is non-nil, print it out and halt.
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+		os.Exit(1)
 	}
 }
